aa8a93805ad108657e220ca99a5ecc3edd5734dd
Mephys <sascha_axt@web.de>
2013-05-30 22:18:56 +0200

    Added new overloads. Every function returns a new offset now.

--- code/engine/Core/CharArray.h
+++ code/engine/Core/CharArray.h
@@ -33,6 +33,10 @@ along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
 #include <boost/utility/enable_if.hpp>
 #include <boost/type_traits/is_pointer.hpp>
 
+#include <Core/Types.h>
+#include <Core/v3.h>
+#include <Core/qv4.h>
+
 typedef boost::array<char,128> CharArray128T;
 typedef boost::array<char,512> CharArray512T;
 
@@ -46,16 +50,73 @@ boost::array<char, ArraySize> stringToArray(const std::string& s)
 }
 
 template <typename T, typename ArrayT>
-void arrayToValue(T& val, const ArrayT& array, size_t offset, typename boost::disable_if<boost::is_pointer<T> >::type* /*dummy*/ = 0)
+BFG::u32 arrayToValue(T& val, const ArrayT& array, size_t offset, typename boost::disable_if<boost::is_pointer<T> >::type* /*dummy*/ = 0)
 {
 	memcpy(&val, &array[offset], sizeof(T));
+	return offset + sizeof(T);
+}
+
+template <typename ArrayT>
+BFG::u32 arrayToValue(std::string& val, const ArrayT& array, size_t offset)
+{
+	BFG::u32 stringSize;
+	BFG::u32 newOffset = arrayToValue(stringSize, array, offset);
+	std::copy(array.begin()+newOffset, array.begin()+newOffset+stringSize, std::back_inserter(val));
+	return newOffset + stringSize;
+}
+
+template <typename ArrayT>
+BFG::u32 arrayToValue(BFG::v3& val, const ArrayT& array, size_t offset)
+{
+	BFG::u32 newOffset = arrayToValue(val.x, array, offset);
+	newOffset = arrayToValue(val.y, array, newOffset);
+	return arrayToValue(val.z, array, newOffset);
+}
+
+template <typename ArrayT>
+BFG::u32 arrayToValue(BFG::qv4& val, const ArrayT& array, size_t offset)
+{
+	BFG::u32 newOffset = arrayToValue(val.w, array, offset);
+	newOffset = arrayToValue(val.x, array, newOffset);
+	newOffset = arrayToValue(val.y, array, newOffset);
+	return arrayToValue(val.z, array, newOffset);
 }
 
 template <typename T, typename ArrayT>
-void valueToArray(const T& val, ArrayT& array, const size_t offset)
+BFG::u32 valueToArray(const T& val, ArrayT& array, const size_t offset)
 {
 	assert( sizeof(T) <= array.size() - offset );
 	memcpy(array.data() + offset, &val, sizeof(T));
+	return offset + sizeof(T);
 }
+
+template <typename ArrayT>
+BFG::u32 valueToArray(const std::string& val, ArrayT& array, const size_t offset)
+{
+	assert( val.size() <= array.size() - offset );
+	memcpy(array.data() + offset, val.c_str(), val.size());
+	return offset + val.size();
+}
+
+template <typename ArrayT>
+BFG::u32 valueToArray(const BFG::v3& val, ArrayT& array, const size_t offset)
+{
+	BFG::u32 newOffset = valueToArray(val.x, array, offset);
+	newOffset = valueToArray(val.y, array, newOffset);
+	newOffset = valueToArray(val.z, array, newOffset);
+	return newOffset;
+}
+
+template <typename ArrayT>
+BFG::u32 valueToArray(const BFG::qv4& val, ArrayT& array, const size_t offset)
+{
+	BFG::u32 newOffset = valueToArray(val.w, array, offset);
+	newOffset = valueToArray(val.x, array, newOffset);
+	newOffset = valueToArray(val.y, array, newOffset);
+	newOffset = valueToArray(val.z, array, newOffset);
+	return newOffset;
+}
+
+
 
 #endif
