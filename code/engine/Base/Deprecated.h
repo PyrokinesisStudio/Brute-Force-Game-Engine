5cd512cf5528a70d3e3b6a19219c11c04c9c0a4a
Sascha Wittkowski <wittus@gmx.de>
2013-06-08 21:33:05 +0200

    documented BFG_DEPRECATED

--- code/engine/Base/Deprecated.h
+++ code/engine/Base/Deprecated.h
@@ -34,11 +34,37 @@ along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
 	#define BFG_DEPRECATED_START(text)
 	#define BFG_DEPRECATED_END __attribute__((deprecated))
 #else
+	//! Helper macro for \ref BFG_DEPRECATED
 	#define BFG_DEPRECATED_START
+	//! Helper macro for \ref BFG_DEPRECATED
 	#define BFG_DEPRECATED_END
 	#pragma message("WARNING: You need to implement BFG_DEPRECATED for this compiler")
 #endif
 
+//! Marks a function as deprecated.
+/**
+	This will emit a compiler warning and generate a deprecated tag for doxygen.
+
+	The following must be defined within the Doxyfile in order to generate
+	deprecated tags:
+
+	\code
+	PREDEFINED = "BFG_DEPRECATED(text)=START_COMMENT \deprecated text END_COMMENT"
+	\endcode
+
+	Note that START_COMMENT and END_COMMENT must be replaced by C++ comment
+	preprocessor instructions.
+
+	Example of usage within C++ code:
+
+	\code
+	BFG_DEPRECATED("Use: Event::id()")
+	void getId()
+	{
+		return id;
+	}
+	\endcode
+*/
 #define BFG_DEPRECATED(text) BFG_DEPRECATED_START(text)BFG_DEPRECATED_END
 
 #endif
