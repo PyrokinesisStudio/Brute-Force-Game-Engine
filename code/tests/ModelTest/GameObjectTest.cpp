320cf01d0b09708d0fcf9fac9cc1045865920418
Andreas Pfirrmann <andreaspfirrmann@gmx.de>
2013-08-05 14:39:40 +0200

    added new model unit tests.

--- code/tests/ModelTest/GameObjectTest.cpp
+++ code/tests/ModelTest/GameObjectTest.cpp
@@ -1,7 +1,34 @@
+/*    ___  _________     ____          __         
+     / _ )/ __/ ___/____/ __/___ ___ _/_/___ ___ 
+    / _  / _// (_ //___/ _/ / _ | _ `/ // _ | -_)
+   /____/_/  \___/    /___//_//_|_, /_//_//_|__/ 
+                               /___/             
+
+This file is part of the Brute-Force Game Engine, BFG-Engine
+
+For the latest info, see http://www.brute-force-games.com
+
+Copyright (c) 2013 Brute-Force Games GbR
+
+The BFG-Engine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The BFG-Engine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <Model/GameObject.h>
 
-#define BOOST_TEST_MODULE GameObjectTest
+#define BOOST_TEST_MODULE ModelTest
 #include <boost/test/unit_test.hpp>
+BOOST_AUTO_TEST_SUITE(GameObjectTestSuite)
 
 BOOST_AUTO_TEST_CASE (VectorToModuleFromRootTest)
 {
@@ -54,3 +81,4 @@ BOOST_AUTO_TEST_CASE (VectorToModuleFromRootTest)
 	BOOST_REQUIRE (positionCorrect && orientationCorrect);
 }
 
+BOOST_AUTO_TEST_SUITE_END()
