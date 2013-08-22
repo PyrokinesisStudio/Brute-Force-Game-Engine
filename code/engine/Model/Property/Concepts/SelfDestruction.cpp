1df89e67489158b686aea7478e2b657a87f5296c
Andreas Pfirrmann <andreaspfirrmann@gmx.de>
2013-08-22 17:37:44 +0200

    fixed destruction emit in SelfDestruction

--- code/engine/Model/Property/Concepts/SelfDestruction.cpp
+++ code/engine/Model/Property/Concepts/SelfDestruction.cpp
@@ -66,7 +66,7 @@ void SelfDestruction::internalSynchronize()
 {
 	while (!mToDestroy.empty())
 	{
-		subLane()->emit(ID::PE_CONTACT, 999999.0f, ownerHandle());
+		subLane()->emit(ID::PE_CONTACT, boost::make_tuple(ownerHandle(), ownerHandle(), 999999.0f), ownerHandle());
 		mToDestroy.pop();
 	}	// TODO: separate calculation and emitting
 	
