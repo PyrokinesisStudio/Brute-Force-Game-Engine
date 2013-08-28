d23a1af2ad5c4658a202834d03f37f2e34aa7a8e
Sascha Wittkowski <wittus@gmx.de>
2013-08-28 15:25:53 +0200

    improved cpp11test and added c++11's unique_ptr

--- code/tests/BaseTest/Cpp11Test.cpp
+++ code/tests/BaseTest/Cpp11Test.cpp
@@ -6,35 +6,37 @@ struct Cpp11Notification
 {
 	Cpp11Notification()
 	{
-		std::cout << "C++11 features enabled. Version:" << __cplusplus << std::endl;
+		std::cout << "C++11 features enabled. Version: " << __cplusplus << std::endl;
 	}
 };
 
-//! Testing move semantics: An object which can only be moved, but not copied.
-struct Object : boost::noncopyable
+//! Testing move semantics with an object which can only be moved, but not
+//! copied.
+struct NonCopyableObject : boost::noncopyable
 {
-	Object(const std::string& msg) : msg(msg) {}
-	Object(Object&& o) : msg(std::move(o.msg)) {}
-	Object& operator = (Object&& o){ msg = std::move(o.msg); return *this; }
+	NonCopyableObject(const std::string& msg) : msg(msg) {}
+	NonCopyableObject(NonCopyableObject&& o) : msg(std::move(o.msg)) {}
+	NonCopyableObject& operator = (NonCopyableObject&& o){ msg = std::move(o.msg); return *this; }
 	std::string msg;
 };
 
-//! Testing move semantics: Creates an 'Object' and returns it by moving it back.
-Object createObject()
+//! Testing move semantics: Creates a 'NonCopyableObject' and returns it by
+//! moving it back.
+NonCopyableObject createNonCopyableObject()
 {
-	return std::move(Object("Testing move semantics"));
+	return std::move(NonCopyableObject("Testing move semantics"));
 }
 
-BOOST_GLOBAL_FIXTURE (Cpp11Notification);
+BOOST_GLOBAL_FIXTURE (Cpp11Notification)
 
-BOOST_AUTO_TEST_SUITE(Cpp11TestSuite)
+BOOST_AUTO_TEST_SUITE(Cpp11LanguageTests)
 
 BOOST_AUTO_TEST_CASE (testAuto)
 {
 	std::vector<int> v;
 	v.push_back(5);
 	auto it = v.begin();
-	it = it;
+	std::cout << "C++11 auto test: " << *it << std::endl;
 }
 
 BOOST_AUTO_TEST_CASE (testLambda)
@@ -46,8 +48,8 @@ BOOST_AUTO_TEST_CASE (testLambda)
 
 BOOST_AUTO_TEST_CASE (testMoveSemantics)
 {
-	std::vector<Object> v;
-	v.emplace_back(createObject());
+	std::vector<NonCopyableObject> v;
+	v.emplace_back(createNonCopyableObject());
 	BOOST_CHECK_EQUAL(v.size(), 1);
 	BOOST_CHECK(v[0].msg == "Testing move semantics");
 }
@@ -65,3 +67,23 @@ BOOST_AUTO_TEST_CASE (testStaticAssert)
 
 BOOST_AUTO_TEST_SUITE_END()
 
+BOOST_AUTO_TEST_SUITE(Cpp11LibraryTests)
+
+BOOST_AUTO_TEST_CASE (testUniquePtr)
+{
+	// Create a unique_ptr
+	std::unique_ptr<int> i(new int(1337));
+	std::cout << "unique_ptr i: " << *i << std::endl;
+	
+	// Now try transfer-of-ownership to new pointer
+	std::unique_ptr<int> j = std::move(i);
+	std::cout << "unique_ptr j: " << *j << std::endl;
+	
+	// Old pointer is useless.
+	BOOST_CHECK(i.get() == nullptr);
+	
+	// New pointer contains value
+	BOOST_CHECK_EQUAL(*j, 1337);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
