d5b68af9d6c08168a109fa49c203f0fb6bf218ef
Mephys <sascha_axt@web.de>
2013-11-29 20:03:39 +0100

    Tests: New testcases for stringtest.

---
+++ code/tests/CoreTest/StringTest.cpp
@@ -0,0 +1,70 @@
+
+#include <Core/String.h>
+
+
+#include <boost/test/unit_test.hpp>
+BOOST_AUTO_TEST_SUITE(StringTest)
+
+using namespace BFG;
+
+BOOST_AUTO_TEST_CASE(testSplit)
+{
+    std::string token;
+    std::string rest;
+    std::string source = "first second third fourth";
+    bool res = false;
+    
+    res = split(source, " ", token, rest);
+    BOOST_CHECK_EQUAL(res, true);
+    BOOST_CHECK_EQUAL(token, "first");
+}
+
+BOOST_AUTO_TEST_CASE(testSplit2)
+{
+    std::string token;
+    std::string rest;
+    std::string source = "first-*-second-*-third-*-fourth";
+    bool res = false;
+    
+    res = split(source, "-*-", token, rest);
+    BOOST_CHECK_EQUAL(res, true);
+    BOOST_CHECK_EQUAL(token, "first");
+}
+
+BOOST_AUTO_TEST_CASE(testSplitNegative)
+{
+    std::string token;
+    std::string rest;
+    std::string source = "firstSecond";
+    bool res = true;
+    
+    res = split(source, " ", token, rest);
+    BOOST_CHECK_EQUAL(res, false);
+    BOOST_CHECK_EQUAL(token, "");
+}
+
+BOOST_AUTO_TEST_CASE(testSplitAll)
+{
+    std::string source = "first second third fourth";
+    std::vector<std::string> tokens;
+    splitComplete(source, " ", tokens);
+    BOOST_CHECK_EQUAL(tokens.empty(), false);
+    BOOST_CHECK_EQUAL(tokens[0], "first");
+    BOOST_CHECK_EQUAL(tokens[1], "second");
+    BOOST_CHECK_EQUAL(tokens[2], "third");
+    BOOST_CHECK_EQUAL(tokens[3], "fourth");
+}
+
+BOOST_AUTO_TEST_CASE(testSplitAll2)
+{
+    std::string source = "first-*-second-*-third-*-fourth";
+    std::vector<std::string> tokens;
+    splitComplete(source, "-*-", tokens);
+    BOOST_CHECK_EQUAL(tokens.empty(), false);
+    BOOST_CHECK_EQUAL(tokens[0], "first");
+    BOOST_CHECK_EQUAL(tokens[1], "second");
+    BOOST_CHECK_EQUAL(tokens[2], "third");
+    BOOST_CHECK_EQUAL(tokens[3], "fourth");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
