26bc30587cf91e8b044a37f1ad7df8efe2d4f0d7
Mephys <sascha_axt@web.de>
2013-12-05 19:47:32 +0100

    CoreTest: Adjusted tests.

--- code/tests/CoreTest/StringTest.cpp
+++ code/tests/CoreTest/StringTest.cpp
@@ -15,6 +15,7 @@ BOOST_AUTO_TEST_CASE(testSplit)
     bool res = false;
     
     res = split(source, " ", token, rest);
+	BOOST_CHECK(!token.empty());
     BOOST_CHECK_EQUAL(res, true);
     BOOST_CHECK_EQUAL(token, "first");
 }
@@ -26,7 +27,8 @@ BOOST_AUTO_TEST_CASE(testSplit2)
     std::string source = "first-*-second-*-third-*-fourth";
     bool res = false;
     
-    res = split(source, "-*-", token, rest);
+    res = split(source, "-\*-", token, rest);
+	BOOST_CHECK(!token.empty());
     BOOST_CHECK_EQUAL(res, true);
     BOOST_CHECK_EQUAL(token, "first");
 }
@@ -48,7 +50,8 @@ BOOST_AUTO_TEST_CASE(testSplitAll)
     std::string source = "first second third fourth";
     std::vector<std::string> tokens;
     tokenize(source, " ", tokens);
-    BOOST_CHECK_EQUAL(tokens.empty(), false);
+    BOOST_CHECK(!tokens.empty());
+	BOOST_CHECK_EQUAL(tokens.size(), 4);
     BOOST_CHECK_EQUAL(tokens[0], "first");
     BOOST_CHECK_EQUAL(tokens[1], "second");
     BOOST_CHECK_EQUAL(tokens[2], "third");
@@ -60,7 +63,8 @@ BOOST_AUTO_TEST_CASE(testSplitAll2)
     std::string source = "first-*-second-*-third-*-fourth";
     std::vector<std::string> tokens;
     tokenize(source, "-\*-", tokens);
-    BOOST_CHECK_EQUAL(tokens.empty(), false);
+    BOOST_CHECK(!tokens.empty());
+	BOOST_CHECK_EQUAL(tokens.size(), 4);
     BOOST_CHECK_EQUAL(tokens[0], "first");
     BOOST_CHECK_EQUAL(tokens[1], "second");
     BOOST_CHECK_EQUAL(tokens[2], "third");
