d007811f3e2bddd644b57b7a25fc0bcb5950312d
Mephys <sascha_axt@web.de>
2013-12-14 17:15:18 +0100

    Added new Console.

---
+++ code/engine/Core/Console.cpp
@@ -0,0 +1,123 @@
+/*    ___  _________     ____          __         
+     / _ )/ __/ ___/____/ __/___ ___ _/_/___ ___ 
+    / _  / _// (_ //___/ _/ / _ | _ `/ // _ | -_)
+   /____/_/  \___/    /___//_//_|_, /_//_//_|__/ 
+                               /___/             
+
+This file is part of the Brute-Force Game Engine, BFG-Engine
+
+For the latest info, see http://www.brute-force-games.com
+
+Copyright (c) 2013 Brute-Force Games GbR
+
+The BFG-Engine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The BFG-Engine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <Core/Console.h>
+
+#include <Base/Logger.h>
+#include <assert.h>
+
+
+namespace BFG {
+
+Command::Command(function_ptr fptr) : mFunction(fptr) {}
+	
+void Command::add(std::deque<std::string> tokens, function_ptr ptr)
+{
+	auto it = mCommands.find(tokens.front());
+
+	// last token means no further subcommand.
+	if (tokens.size() == 1)
+	{
+		// No double definition of command is allowed.
+		assert(it == mCommands.end());
+        
+		mCommands[tokens[0]] = boost::shared_ptr<Command>(new Command(ptr));
+		return;
+	}
+        
+	// No such command already exits. Add a new one and proceed with next subcommand.
+	if (it == mCommands.end())
+	{
+		boost::shared_ptr<Command> tmpCmd(new Command);
+		mCommands[tokens.front()] = tmpCmd;
+		tokens.pop_front();
+		tmpCmd->add(tokens, ptr);
+		return;
+	}
+
+	// Command already exists. Proceed with next subcommand.
+	tokens.pop_front();
+	it->second->add(tokens, ptr);
+}
+
+void Command::execute(const std::string& commandLine)
+{
+	std::string out;
+	std::string rest;
+
+	// Eat next expression.
+	split(commandLine, " ", out, rest);
+
+	// Probably a one expression command.
+	if (out.empty())
+		out = commandLine;
+
+	auto cmd = mCommands.find(out);
+
+	// Another subcommand found. Go on.
+	if (cmd != mCommands.end())
+	{
+		cmd->second->execute(rest);
+		return;
+	}
+	
+	// Expression not found.
+	if (!mFunction)
+		infolog << "Unknown Command!"; // Dead end.
+	else
+		mFunction(commandLine); // Good end.
+}
+
+
+
+void registerCommand(const std::string& command, function_ptr ptr, Command& commands)
+{
+    std::deque<std::string> tokens;
+	tokenize(command, " ", tokens);
+    assert(!tokens.empty());
+    
+    commands.add(tokens, ptr);
+}
+
+
+Console::Console(CommandPtrT commands, std::vector<s32> events, Event::SubLanePtr sublane): 
+	mCommands(commands),
+	mSubLane(sublane)
+{
+    for(auto it = events.begin(); it != events.end(); ++it)
+		mSubLane->connect(*it, this, &Console::onCommand);
+}
+
+
+void Console::onCommand(const std::string& line)
+{
+    if (line.empty())
+        return;
+ 
+	mCommands->execute(line);
+}
+
+} // namespace BFG
---
+++ code/engine/Core/Console.h
@@ -0,0 +1,84 @@
+/*    ___  _________     ____          __         
+     / _ )/ __/ ___/____/ __/___ ___ _/_/___ ___ 
+    / _  / _// (_ //___/ _/ / _ | _ `/ // _ | -_)
+   /____/_/  \___/    /___//_//_|_, /_//_//_|__/ 
+                               /___/             
+
+This file is part of the Brute-Force Game Engine, BFG-Engine
+
+For the latest info, see http://www.brute-force-games.com
+
+Copyright (c) 2013 Brute-Force Games GbR
+
+The BFG-Engine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The BFG-Engine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the BFG-Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BFG_CONSOLE_H__
+#define BFG_CONSOLE_H__
+
+#include <string>
+#include <map>
+#include <deque>
+
+#include <boost/shared_ptr.hpp>
+
+#include <Core/String.h>
+#include <Event/Event.h>
+
+
+namespace BFG {
+
+typedef void (*function_ptr)(const std::string&);
+
+struct Command;
+typedef boost::shared_ptr<Command> CommandPtrT;
+typedef std::map<std::string, CommandPtrT > CommandsT;
+
+//! \brief Recursive structure for console commands.
+struct BFG_CORE_API Command
+{
+	Command(function_ptr fptr = nullptr);
+	
+	void add(std::deque<std::string> tokens, function_ptr ptr);
+	void execute(const std::string& commandLine);
+
+	//! A console command function.
+	function_ptr mFunction;
+	
+	CommandsT mCommands;
+};
+
+
+//! \brief Helper function to define commands.
+//! Syntax: registerCommand("Com1 Com2 Com3", &Com3, commands);
+void BFG_CORE_API registerCommand(const std::string& command, function_ptr ptr, Command& commands);
+
+
+//! \brief Receives events for and connect them with a particular command function.
+class BFG_CORE_API Console
+{
+public:
+    Console(CommandPtrT commands, std::vector<s32> events, Event::SubLanePtr sublane);
+
+private:
+
+    void onCommand(const std::string& line);
+	
+	CommandPtrT mCommands;
+    Event::SubLanePtr mSubLane;
+};
+
+} // namespace BFG
+
+#endif
